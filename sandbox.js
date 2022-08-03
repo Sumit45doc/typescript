3427001dc2db4de6259892dc9eec9a3ee1c8e266
Sumit Kumar Sharma <Sumit.SHARMA@3ds.com>
2022-08-03 16:02:07 +0530

    types in tsc

--- sandbox.js
+++ sandbox.js
@@ -1,3 +1,15 @@
-var toPerson = 'sumit kumar sharma';
+var toPerson;
+toPerson = 'sumit kumar sharma';
 var elem = document.getElementById('enter');
 elem.innerText = toPerson;
+var circ = function (diameter, circumference) {
+    if (circumference)
+        return circumference;
+    return diameter * Math.PI;
+};
+console.log(circ(5, 2));
+// types
+// boolean
+// number
+// string
+// object
--- sandbox.ts
+++ sandbox.ts
@@ -1,3 +1,19 @@
-const toPerson = 'sumit kumar sharma';
+let toPerson: string;
+toPerson = 'sumit kumar sharma';
 const elem = document.getElementById('enter') as HTMLDivElement;
 elem.innerText = toPerson;
+
+
+const circ = (diameter: number, circumference?: number): number => {
+    if(circumference) return circumference
+    return diameter * Math.PI;
+}
+
+
+console.log(circ(5, 2))
+
+// types
+// boolean
+// number
+// string
+// object
