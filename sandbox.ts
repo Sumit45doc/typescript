22862f5abb554e1a30c4aa0643adf494ca5948ed
Sumit Kumar Sharma <Sumit.SHARMA@3ds.com>
2022-08-03 16:09:31 +0530

    array

--- sandbox.ts
+++ sandbox.ts
@@ -17,3 +17,24 @@ console.log(circ(5, 2))
 // number
 // string
 // object
+
+
+
+// array
+
+const num: number[] = [2, 3, 5];
+num.push(5);
+num[0] = 5;
+console.log("num ", num);
+
+const str: string[] = ["apple", "ball"];
+str.push("coconut");
+console.log("str ", str);
+
+const selectedType: (string | number)[] = ["str", 5];
+selectedType.push("boolean or other type not allowed");
+console.log("selectedType ", selectedType);
+
+const mix: any[] = ["all", "mixed", 5];
+mix.push(true);
+console.log("mix ", mix);
